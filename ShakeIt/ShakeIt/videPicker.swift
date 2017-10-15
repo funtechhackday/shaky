e352c188114000fc7a8da278244469a4fffdf618
Bandd-k <banddk1@gmail.com>
2017-10-15 10:24:11 +0300

    first commit

---
+++ ShakeIt/ShakeIt/videPicker.swift
@@ -0,0 +1,13 @@
+//
+//  videPicker.swift
+//  ShakeIt
+//
+//  Created by Denis Karpenko on 27.04.16.
+//  Copyright © 2016 Denis Karpenko. All rights reserved.
+//
+
+import UIKit
+
+class videPicker: NSObject {
+
+}
